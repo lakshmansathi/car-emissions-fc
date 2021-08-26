f4706bf34c40d92252f8ac6ae39da80d7cefe97d
lakshmansathi <66879638+lakshmansathi@users.noreply.github.com>
2021-08-26 19:05:13 -0400

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -36,7 +36,13 @@ st.write('---')
 X = data[['manufacturer','transmission','engine_capacity','noise_level','euro_standard_','Petrol','Diesel','Petrol Hybrid']]
 X["engine_capacity"] = pd.to_numeric(X["engine_capacity"], downcast="float")
 X["noise_level"] = pd.to_numeric(X["noise_level"], downcast="float")
-X["euro_standard_"] = pd.to_numeric(X["euro_standard_"], downcast="float")# Sidebar
+X["euro_standard_"] = pd.to_numeric(X["euro_standard_"], downcast="float")
+X["manufacturer"] = pd.to_numeric(X["manufacturer"], downcast="float")
+X["transmission"] = pd.to_numeric(X["transmission"], downcast="float")
+X["Petrol"] = pd.to_numeric(X["Petrol"], downcast="float")
+X["Diesel"] = pd.to_numeric(X["Diesel"], downcast="float")
+X["Petrol Hybrid'"] = pd.to_numeric(X["Petrol Hybrid"], downcast="float")
+
 st.sidebar.header('Specify Input Parameters')
 def user_input_features():
     Engine_Capacity = st.sidebar.slider('engine Capacity', X.engine_capacity.min(), X.engine_capacity.max(), X.engine_capacity.mean())
